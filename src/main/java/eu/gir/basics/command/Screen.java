15205d90756afacce123d70a07e24148bd78790b
FGBxRamel <kretschmer.raziel@gmail.com>
2021-11-30 16:41:26 +0100

    Made 'new'-button work, made title for entity-list

--- src/main/java/eu/gir/basics/command/Screen.java
+++ src/main/java/eu/gir/basics/command/Screen.java
@@ -2,9 +2,10 @@ package eu.gir.basics.command;
 
 import eu.gir.girsignals.guis.guilib.GuiBase;
 import eu.gir.girsignals.guis.guilib.GuiElements.UIButton;
+import eu.gir.girsignals.guis.guilib.GuiElements.UIClickable;
 import eu.gir.girsignals.guis.guilib.GuiElements.UIEntity;
+import eu.gir.girsignals.guis.guilib.GuiElements.UILabel;
 import eu.gir.girsignals.guis.guilib.GuiElements.UIVBox;
-import eu.gir.girsignals.guis.guilib.UIColor;
 
 public class Screen extends GuiBase {
 
@@ -20,46 +21,57 @@ public class Screen extends GuiBase {
 	@Override
 	public void initGui() {
 		super.initGui();
+		
+		UIEntity rightEntity = new UIEntity();
+		UIEntity leftEntity = new UIEntity();
+		UIEntity newButtonEntity = new UIEntity();
+		UIEntity previewButtonEntity = new UIEntity();
+		UIEntity entityList = new UIEntity();
+		UIEntity sidedEntitys = new UIEntity();
 
 		UIVBox leftVbox = new UIVBox(4);
-		UIEntity leftEntity = new UIEntity();
 		leftEntity.setBounds(Math.round(entity.getWidth() / 3), entity.getHeight());
 		leftEntity.add(leftVbox);
-		entity.add(leftEntity);
+//		UIColor leftColor = new UIColor(0xFF0000FF);
+//		leftEntity.add(leftColor);
 
-		UIEntity newButtonEntity = new UIEntity();
 		newButtonEntity.setBounds(35, 20);
 		UIButton newButton = new UIButton("New");
 		newButtonEntity.add(newButton);
+		UIClickable newButtonClickable = new UIClickable(e -> {
+			testButton(entityList);
+		});
+		newButtonEntity.add(newButtonClickable);
 		leftEntity.add(newButtonEntity);
 
-		UIEntity previewButtonEntity = new UIEntity();
 		previewButtonEntity.setBounds(50, 20);
 		UIButton previewButton = new UIButton("Preview");
 		previewButtonEntity.add(previewButton);
 		leftEntity.add(previewButtonEntity);
 
-		UIEntity rightEntity = new UIEntity();
 		rightEntity.setBounds(Math.round(entity.getWidth() / 3 * 2), entity.getHeight());
+		rightEntity.setPos(leftEntity.getWidth(), rightEntity.getY());
+//		UIColor rightColor = new UIColor(0xF00000FF);
+//		rightEntity.add(rightColor);
 
-		UIEntity entityList = new UIEntity();
 		entityList.setBounds(Math.round(rightEntity.getWidth() / 2), rightEntity.getHeight());
 		UIVBox entityListBox = new UIVBox(2);
 		entityList.add(entityListBox);
+		UIEntity entityTitleEntity = new UIEntity();
+		UILabel entityTitle = new UILabel("Entitys");
+		entityTitleEntity.add(entityTitle);
+		entityList.add(entityTitleEntity);
+//		UIColor entityListColor = new UIColor(0xFF0000FF);
+//		entityList.add(entityListColor);
+
 		UIEntity propertyList = new UIEntity();
 		propertyList.setBounds(Math.round(rightEntity.getWidth() / 2), rightEntity.getHeight());
 		propertyList.setPos(propertyList.getX() + Math.round(rightEntity.getWidth() / 2), propertyList.getY());
-//		UIEntity testButtonEntity = new UIEntity();
-//		testButtonEntity.setBounds(60, 20);
-//		UIButton testButton = new UIButton("Testbutton");
-//		testButtonEntity.add(testButton);
-//		propertyList.add(testButtonEntity);
-		testButton(propertyList);
-		testButton(entityList);
+//		UIColor propertyListColor = new UIColor(0xF00000FF);
+//		propertyList.add(propertyListColor);
 		rightEntity.add(propertyList);
 		rightEntity.add(entityList);
 
-		UIEntity sidedEntitys = new UIEntity();
 		sidedEntitys.add(rightEntity);
 		sidedEntitys.add(leftEntity);
 		entity.add(sidedEntitys);
